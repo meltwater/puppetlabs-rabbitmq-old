9cb59fdde40ff52274160b4c28fccb7b48ba1d49
William Leese <william.leese@meltwater.com>
2013-05-30 10:31:55 +0900

    Puppet 3

--- lib/puppet/provider/rabbitmq_plugin/rabbitmqplugins.rb
+++ lib/puppet/provider/rabbitmq_plugin/rabbitmqplugins.rb
@@ -1,6 +1,6 @@
 Puppet::Type.type(:rabbitmq_plugin).provide(:rabbitmqplugins) do
 
-  commands :rabbitmqplugins => 'rabbitmq-plugins'
+  commands :rabbitmqplugins => 'HOME="/root" rabbitmq-plugins'
   defaultfor :feature => :posix
 
   def self.instances
